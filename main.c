a828a265f99e4ea00244a120835adf5ebebbff4e
Juan Puelpan <juan@puelpan.com>
2025-12-28 02:40:18 -0300

    refactor: split render time fragment and time separators

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ SDL_Renderer *renderer = NULL;
 SDL_Texture *whiteTextures[60];
 SDL_Texture *blackTextures[60];
 
+SDL_Color fgColor = {0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE};
+SDL_Color bgColor = {0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE};
+
 void render_grid(int *width, int *height) {
   float col_width = (float)*width / (float)GRID_COUNT;
 
@@ -33,13 +36,82 @@ void render_grid(int *width, int *height) {
   }
 }
 
+void draw_circle(int x0, int y0, int r) {
+  int x = r - 1;
+  int y = 0;
+  int dx = 1;
+  int dy = 1;
+  int err = dx - (r << 1);
+
+  while (x >= y) {
+    SDL_RenderLine(renderer, x + x0, y + y0, -x + x0, y + y0);
+    SDL_RenderLine(renderer, y + x0, x + y0, -y + x0, x + y0);
+    SDL_RenderLine(renderer, -x + x0, -y + y0, x + x0, -y + y0);
+    SDL_RenderLine(renderer, -y + x0, -x + y0, y + x0, -x + y0);
+
+    if (err <= 0) {
+      y++;
+      err += dy;
+      dy += 2;
+    }
+
+    if (err > 0) {
+      x--;
+      dx += 2;
+      err += dx - (r << 1);
+    }
+  }
+}
+
+void render_time_fragment(int *tm_part, SDL_FRect rect, int w, int h) {
+  float shadow_x = ((w * SHADOW_OFFSET_X) / 100);
+  float shadow_y = ((h * SHADOW_OFFSET_Y) / 100);
+
+  SDL_FRect shadowRect;
+  shadowRect.w = rect.w;
+  shadowRect.h = rect.h;
+  shadowRect.x = rect.x + shadow_x;
+  shadowRect.y = rect.y + shadow_y;
+
+  // Render shadow first
+  SDL_SetRenderDrawColor(renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+  SDL_RenderTexture(renderer, blackTextures[*tm_part], NULL, &shadowRect);
+
+  // Render text later
+  SDL_SetRenderDrawColor(renderer, fgColor.r, fgColor.g, fgColor.b, fgColor.a);
+  SDL_RenderTexture(renderer, whiteTextures[*tm_part], NULL, &rect);
+}
+
+void render_time_separator(int x, float block_height, float w, float h) {
+  float shadow_x = ((w * SHADOW_OFFSET_X) / 100.0f);
+  float shadow_y = ((h * SHADOW_OFFSET_Y) / 100.0f);
+
+  // Top circle
+  SDL_SetRenderDrawColor(renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+  draw_circle(x + shadow_x, block_height - (block_height / 9.0f) + shadow_y,
+              ((w * 1.5f) / 100.0f));
+
+  SDL_SetRenderDrawColor(renderer, fgColor.r, fgColor.g, fgColor.b, fgColor.a);
+  draw_circle(x, block_height - (block_height / 9.0f), ((w * 1.5f) / 100.0f));
+
+  // Bottom circle
+  SDL_SetRenderDrawColor(renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+  draw_circle(x + shadow_x, block_height + (block_height / 9.0f) + shadow_y,
+              ((w * 1.5f) / 100.0f));
+
+  SDL_SetRenderDrawColor(renderer, fgColor.r, fgColor.g, fgColor.b, fgColor.a);
+  draw_circle(x, block_height + (block_height / 9.0f), ((w * 1.5f) / 100.0f));
+}
+
 int main(int argc, char *argv[]) {
   uint32_t win_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_TRANSPARENT |
                        SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS;
 
+  bool isFullScreen = false;
   for (int i = 0; i < argc; i++) {
     if (strcmp(argv[i], "-fullscreen") == 0) {
       win_flags = win_flags | SDL_WINDOW_FULLSCREEN;
+      isFullScreen = true;
     }
   }
 
@@ -62,14 +134,11 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  SDL_Color white = {0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE};
-  SDL_Color black = {0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE};
-
   for (int i = 0; i < 60; i++) {
     char content[10];
     sprintf(content, "%02d", i);
 
-    SDL_Surface *text = TTF_RenderText_Blended(font, content, 0, white);
+    SDL_Surface *text = TTF_RenderText_Blended(font, content, 0, fgColor);
     if (!text) {
       SDL_Log("Failed to texturize number: %s\n", SDL_GetError());
       return 1;
@@ -77,7 +146,7 @@ int main(int argc, char *argv[]) {
 
     whiteTextures[i] = SDL_CreateTextureFromSurface(renderer, text);
 
-    text = TTF_RenderText_Blended(font, content, 0, black);
+    text = TTF_RenderText_Blended(font, content, 0, bgColor);
     if (!text) {
       SDL_Log("Failed to texturize number: %s\n", SDL_GetError());
       return 1;
@@ -87,8 +156,8 @@ int main(int argc, char *argv[]) {
   }
 
   int canvas_width = 0, canvas_height = 0;
-
   bool shouldLoop = true;
+
   while (shouldLoop) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -96,6 +165,9 @@ int main(int argc, char *argv[]) {
       case SDL_EVENT_KEY_DOWN:
         if (event.key.key == SDLK_ESCAPE) {
           shouldLoop = false;
+        } else if (event.key.key == SDLK_F) {
+          isFullScreen = !isFullScreen;
+          SDL_SetWindowFullscreen(window, isFullScreen);
         }
         break;
       case SDL_EVENT_QUIT:
@@ -105,9 +177,8 @@ int main(int argc, char *argv[]) {
     }
 
     SDL_GetRenderOutputSize(renderer, &canvas_width, &canvas_height);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0x00); // transparent window
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_TRANSPARENT);
     SDL_RenderClear(renderer);
-
     /* render_grid(&canvas_width, &canvas_height); */
 
     time_t now;
@@ -117,45 +188,24 @@ int main(int argc, char *argv[]) {
     float col_width = (float)canvas_width / (float)GRID_COUNT;
     float block_height = canvas_height / 2.0;
 
-    SDL_FRect rect, shadowRect;
+    SDL_FRect rect;
+    rect.x = col_width * 2;
     rect.w = col_width * 3;
     rect.h = block_height;
     rect.y = canvas_height / 2.0 - block_height / 2.0;
 
-    shadowRect.w = rect.w;
-    shadowRect.h = rect.h;
-
-    float shadow_x = ((canvas_width * SHADOW_OFFSET_X) / 100);
-    float shadow_y = ((canvas_height * SHADOW_OFFSET_Y) / 100);
-
     rect.x = col_width * 2;
-    shadowRect.x = rect.x + shadow_x;
-    shadowRect.y = rect.y + shadow_y;
-
-    // Render shadow first
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
-    SDL_RenderTexture(renderer, blackTextures[now_tm->tm_hour], NULL,
-                      &shadowRect);
-
-    // Render text later
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
-    SDL_RenderTexture(renderer, whiteTextures[now_tm->tm_hour], NULL, &rect);
+    render_time_fragment(&now_tm->tm_hour, rect, canvas_width, canvas_height);
+    render_time_separator((rect.x + rect.w + col_width / 2), block_height,
+                          canvas_width, canvas_height);
 
     rect.x = rect.x + col_width * 4.0;
-    shadowRect.x = rect.x + shadow_x;
-    shadowRect.y = rect.y + shadow_y;
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
-    SDL_RenderTexture(renderer, blackTextures[now_tm->tm_min], NULL,
-                      &shadowRect);
-    SDL_RenderTexture(renderer, whiteTextures[now_tm->tm_min], NULL, &rect);
+    render_time_fragment(&now_tm->tm_min, rect, canvas_width, canvas_height);
+    render_time_separator((rect.x + rect.w + col_width / 2), block_height,
+                          canvas_width, canvas_height);
 
     rect.x = rect.x + col_width * 4.0;
-    shadowRect.x = rect.x + shadow_x;
-    shadowRect.y = rect.y + shadow_y;
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
-    SDL_RenderTexture(renderer, blackTextures[now_tm->tm_sec], NULL,
-                      &shadowRect);
-    SDL_RenderTexture(renderer, whiteTextures[now_tm->tm_sec], NULL, &rect);
+    render_time_fragment(&now_tm->tm_sec, rect, canvas_width, canvas_height);
 
     SDL_RenderPresent(renderer);
     SDL_Delay(100);
